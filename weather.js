96ddf7753ce600ab1d9ca11aae85de7cc296a369
luna <tactical-waffle@outlook.com>
2022-07-19 13:18:00 +0100

    made the api request use https not http

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const weather = document.querySelector('#weather');
 
 const getWeather = async () => {
-	const res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
+	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
 	const json = await res.json();
 	console.log(json.main.temp);
 	var weatherText = Math.round(json.main.temp - 273.15);
