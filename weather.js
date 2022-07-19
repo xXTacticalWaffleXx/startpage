0c764235b9351050af5122181d4d4265655bb7a7
luna <tactical-waffle@outlook.com>
2022-07-19 06:58:07 +0100

    got some code from my mate cause i fucked it

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
-document.getElementById('weather').onload = dingus;
+const weather = document.querySelector('#weather');
 
-function dingus(){
-	document.getElementById('weather').innerHTML = "dighjn";
+function dingus() {
+	weather.innerHTML = "dighjn";
+}
+
+// document.body.onload = dingus;
+
+const getWeather = async () => {
+	const res = await fetch('url to weather');
+	const json = await res.json();
+
+	//optionally parse data from json to take out shit you don't need
+	//update ui with weather data
+
+	//return data if you want idk
 }
