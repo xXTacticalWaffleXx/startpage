37421519278e1a0e5f48400c658918cfadcd34e8
luna <tactical-waffle@outlook.com>
2022-07-19 13:00:17 +0100

    removed un needed code

--- weather.js
+++ weather.js
@@ -1,19 +1,8 @@
 const weather = document.querySelector('#weather');
 
-function dingus() {
-	getWeather();
-}
-
-// document.body.onload = dingus;
-
 const getWeather = async () => {
 	const res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
 	const json = await res.json();
 	console.log(json.main.temp);
 	weather.innerHTML = json.main.temp;
-
-//optionally parse data from json to take out shit you don't need
-	//update ui with weather data
-
-	//return data if you want idk
 }
