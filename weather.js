a67e8c7386afea5373976506168ece4473d027a3
luna <tactical-waffle@outlook.com>
2022-07-20 00:09:19 +0100

    added comments to weather.js

--- weather.js
+++ weather.js
@@ -1,10 +1,12 @@
 const weather = document.querySelector('#weather');
 
 const getWeather = async () => {
+	//get json data from the api
 	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
 	const json = await res.json();
-	console.log(json.main.temp);
+	// get the temperature from the json and convert from kelvin to celcius
 	var weatherText = Math.round(json.main.temp - 273.15);
-	var weatherText = + weatherText + "°C";
+	var weatherText = weatherText + "°C";
+	
 	weather.innerHTML = weatherText;
 	}
