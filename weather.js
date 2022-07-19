96bf404961c651ae15c5fd2b7fcbea9b5ee68a9f
luna <tactical-waffle@outlook.com>
2022-07-19 07:28:00 +0100

    added weather code

--- weather.js
+++ weather.js
@@ -1,14 +1,16 @@
 const weather = document.querySelector('#weather');
 
 function dingus() {
-	weather.innerHTML = "dighjn";
+	weather.innerHTML = getWeather();
 }
 
 // document.body.onload = dingus;
 
 const getWeather = async () => {
-	const res = await fetch('url to weather');
+	const res = await fetch('https://pretty-basic-weather-app.herokuapp.com/api/weather/forecast?city=birmingham&country=GB');
 	const json = await res.json();
+
+	return json.temperature.temp;
 
 	//optionally parse data from json to take out shit you don't need
 	//update ui with weather data
