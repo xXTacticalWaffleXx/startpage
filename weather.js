b84549a5e121991910c1daf3410062184710f340
luna <tactical-waffle@outlook.com>
2022-07-30 19:10:32 +0100

    fixed the site getting weather info for california

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const weather = document.querySelector('#weather');
 
 const getWeather = async () => {
 	//get json data from the api
-	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=California&appid=9eadbebea72109584f66e206ae0996b1');
+	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
 	const json = await res.json();
 	// get the temperature from the json and convert from kelvin to celcius
 	var weatherText = Math.round(json.main.temp - 273.15);
@@ -17,6 +17,10 @@ const getWeather = async () => {
 	else if (json.weather[0].main == "Clear"){	
 	var weatherText = weatherText + " ☀";
 	}
+	
+	//add good [time of day] text
+	
+
 	// add weatherText to the html doc
 	weather.innerHTML = weatherText;
 	}
