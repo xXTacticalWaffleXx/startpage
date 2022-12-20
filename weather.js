c6bd2d81d895c495ca6f299696c6a2f98a83d0e4
Luna Aphelion <luna-aphelion@proton.me>
2022-12-20 03:15:26 +0000

    changed dingu

--- weather.js
+++ weather.js
@@ -18,7 +18,7 @@ const weather = document.querySelector('#weather');
 
 const getWeather = async () => {
 	//get json data from the api
-	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=9eadbebea72109584f66e206ae0996b1');
+	const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Birmingham&appid=9eadbebea72109584f66e206ae0996b1');
 	const json = await res.json();
 	// get the temperature from the json and convert from kelvin to celcius
 	var weatherText = Math.round(json.main.temp - 273.15);
